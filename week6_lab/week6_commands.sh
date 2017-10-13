08507efbafc5840d6cda27a89346497a4ee01831
maxechterling <mechter1@jh.edu>
2017-10-13 17:35:08 -0400

    Add corrections to top 100 diff and commands

--- week6_lab/week6_commands.sh
+++ week6_lab/week6_commands.sh
@@ -12,9 +12,9 @@ macs2 callpeak -t ./sams/CTCF_G1E.sam -c ./sams/input_G1E.sam -g 61431566 -n G1E
 macs2 callpeak -t ./sams/CTCF_ER4.sam -c ./sams/input_ER4.sam -g 61431566 -n ER4
 
 ## find lost peaks (G1E --> ER4)
-bedtools intersect -a ./peaks/G1E_peaks.narrowPeak -b ./peaks/ER4_peaks.narrowPeak > diff_lost.bed
+bedtools intersect -a ./peaks/G1E_peaks.narrowPeak -b ./peaks/ER4_peaks.narrowPeak -v > diff_lost.bed
 ## find gained peaks (G1E --> ER4)
-bedtools intersect -a ./peaks/ER4_peaks.narrowPeak -b ./peaks/G1E_peaks.narrowPeak > diff_gained.bed
+bedtools intersect -a ./peaks/ER4_peaks.narrowPeak -b ./peaks/G1E_peaks.narrowPeak -v > diff_gained.bed
 
 ## get motif sequences
 bedtools getfasta -fi genomes/chr19.fa -bed peaks/G1E_peaks.narrowPeak > G1E.fa
@@ -22,4 +22,4 @@ bedtools getfasta -fi genomes/chr19.fa -bed peaks/ER4_peaks.narrowPeak > ER4.fa
 cat G1E.fa ER4.fa > CTCF_motif.fa
 
 ## run meme
-/usr/local/opt/meme/bin/meme-chip -db motif_databases/* CTCF_motif.fa 
+/usr/local/opt/meme/bin/meme-chip -db motif_databases/ CTCF_motif.fa 
